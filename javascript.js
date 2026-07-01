a392df877a5df6d5dea8f96a38214fc0d594d6c3
Évelyn Caroliny <evycaroliny14@gmail.com>
2026-06-30 21:46:33 -0300

    atividade js

--- javascript.js
+++ javascript.js
@@ -1,33 +1,23 @@
-function somar() {
-   const numero1 = getComputedStyle.ElementById('numero1');
-   const numero2 = getComputedStyle.ElementById('numero2');
+const numero1 = document.getElementById('numero1');
+const numero2 = document.getElementById('numero2');
 
-   document.getElementById('btnSomar').onclick = function
-    const resultado = calcular(Number(numero1.value), Number(numero2.value), '+');
+const btnSomar = document.getElementById('btnSomar');
+const btnSubtrair = document.getElementById('btnSubtrair');
 
-    btnSomar.onclick = function () {
-      const resultado = somar (numero1.value), numero2.value);
-
-      document.getElementById('resultado'). innerText = resultado;
-}
+btnSomar.onclick = function () {
+    const resultado = somar(numero1.value, numero2.value);
+    document.getElementById('resultado').innerText = resultado;
+};
 
 btnSubtrair.onclick = function () {
-  const resultado = somar(numero1.value, numero2.value);
+    const resultado = subtrair(numero1.value, numero2.value);
+    document.getElementById('resultado').innerText = resultado;
+};
 
-  document.getElementById('resultado').innerHTML = resultado;
-}
-
-btnSubtrair.onclick = function () {
-  const resultado = subtrair(numero1.value, numero2.value);
-
-  document.getElementById('resultado').innerHTML = resultado;
-
-}
-
- function somar(numero1,numero2) {
+function somar(num1, num2) {
     return Number(num1) + Number(num2);
- }
+}
 
-  function subtrair(numero1,numero2) {
+function subtrair(num1, num2) {
     return Number(num1) - Number(num2);
- }
+}
